1fe96c9d5cc11ff6ff55ef9ba33905b39d757c43
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-07-14 19:59:37 -0300

    Create media.js

---
+++ 4.5.1/media.js
@@ -0,0 +1,10 @@
+var nota1 = 4;
+var nota2 = 7;
+var nota3 = 5;
+var media = (nota1+nota2+nota3)/3;
+if(media >= 7){
+  alert("Média: " + media +" - Aprovado");
+}
+else{
+	alert("Média: " + media +" - Reprovado");
+}
