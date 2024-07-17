96be6d6bb5cc095c4d7dba103c94bb478dff8094
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-07-17 18:02:10 -0300

    Create load.js

---
+++ 4.12.1/load.js
@@ -0,0 +1,6 @@
+function carreguei(){
+  document.getElementById("log").innerHTML += "carreguei<br>";
+}
+function redimensionei(){
+  document.getElementById("log").innerHTML += "redimensionei<br>";
+}
