1c47afb7a7a00a4e1477bff9a13dc4de74700742
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-07-17 18:25:52 -0300

    Create click.js

---
+++ 4.13.1/click.js
@@ -0,0 +1,8 @@
+function umClique(){
+  document.getElementById("imagem").src="fachada.jpg";
+  document.getElementById("log").innerHTML += "troca para a foto da fachada<br>";
+}
+function doisCliques(){
+  document.getElementById("imagem").src="ifrs.gif";
+  document.getElementById("log").innerHTML += "troca para o logo do IFRS-BG<br>";
+}
